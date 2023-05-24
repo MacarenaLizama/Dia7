70bfb01d6beeaf02eb45e3ba16144b290c68a123
MacarenaLizama <133072411+MacarenaLizama@users.noreply.github.com>
2023-05-24 13:52:02 -0400

    Add files via upload

---
+++ Desafio Bucles/index1.js
@@ -0,0 +1,29 @@
+/*Desafio 1*/
+for(let i=1; i<=20; i++){
+    if(i%2==0){
+        console.log(i);
+    }
+}
+/*Desafio 2*/
+for(let i=100; i>=0; i--){
+    if(i%3==0){
+        console.log(i);
+    }
+}
+/*Desafio 3*/
+for(let i=4; i>=-3.5; i-=1.5){
+    console.log(i);
+}
+/*Desafio 4*/
+let sum=0;
+for(let i=1; i<=100; i++){
+    sum=sum+i;
+}
+console.log(sum);
+
+/*Desafio 5*/
+let product=1;
+for(i=1; i<=12; i++){
+    product=product*i;
+}
+console.log(product);
